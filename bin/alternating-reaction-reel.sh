8fe8eb44a2cb4a4fae6bb7df01ac3ddf0d3dc17e
Vatsal Shah <svatsal64@gmail.com>
2026-09-11 19:32:07 -0400

    Refine Duo reel timing and clean reaction framing

--- bin/alternating-reaction-reel.sh
+++ bin/alternating-reaction-reel.sh
@@ -20,19 +20,20 @@ mkdir -p "$(dirname "$OUT")"
 ffmpeg -nostdin -y -hide_banner -loglevel error \
   -i "$REACTION" -i "$PRODUCT" -i "$AUDIO" \
   -filter_complex "
-    [0:v]fps=30,crop=iw:ih*0.55:0:0,
-      scale=1080:-2,pad=1080:1920:0:(oh-ih)/2:black,setsar=1,
-      eq=contrast=1.04:saturation=0.94[person];
+    [0:v]fps=30,scale=1080:1920,setsar=1,
+      eq=brightness=-0.08:contrast=0.96:saturation=0.92:gamma=0.90[person];
+    [person]split=4[pA][pB][pC][pD];
     [1:v]fps=30,scale=1080:608:force_original_aspect_ratio=decrease,
       pad=1080:1920:(ow-iw)/2:(oh-ih)/2:black,setsar=1,setpts=PTS-STARTPTS[device];
-    [person]trim=start=0.00:end=1.14,setpts=PTS-STARTPTS[p0];
-    [device]trim=start=0.00:end=2.16,setpts=PTS-STARTPTS[p1];
-    [person]trim=start=0.10:end=2.90,setpts=PTS-STARTPTS[p2];
-    [device]trim=start=8.00:end=10.90,setpts=PTS-STARTPTS[p3];
-    [person]trim=start=0.60:end=2.60,setpts=PTS-STARTPTS[p4];
-    [device]trim=start=20.00:end=21.30,setpts=PTS-STARTPTS[p5];
-    [person]trim=start=0.20:end=0.874,setpts=PTS-STARTPTS[p6];
-    [person]trim=start=0.90:end=2.274,setpts=PTS-STARTPTS[p7];
+    [device]split=3[dA][dB][dC];
+    [pA]trim=start=0.00:end=1.14,setpts=PTS-STARTPTS[p0];
+    [dA]trim=start=4.00:end=6.16,setpts=PTS-STARTPTS[p1];
+    [pB]trim=start=0.10:end=2.90,setpts=PTS-STARTPTS[p2];
+    [dB]trim=start=6.00:end=8.90,setpts=PTS-STARTPTS[p3];
+    [pC]trim=start=0.60:end=2.60,setpts=PTS-STARTPTS[p4];
+    [dC]trim=start=10.00:end=11.30,setpts=PTS-STARTPTS[p5];
+    [pD]trim=start=0.20:end=0.874,setpts=PTS-STARTPTS[p6];
+    [pD]trim=start=14.40:end=15.774,setpts=PTS-STARTPTS[p7];
     [p0][p1][p2][p3][p4][p5][p6][p7]concat=n=8:v=1:a=0,trim=0:13.074,
       drawtext=fontfile='${FONT}':text='${TOP1}':fontcolor=white:fontsize=52:
         borderw=3:bordercolor=black@0.65:x=(w-text_w)/2:y=325,
